2e7663fb215529177d526a9b7a833c07aa6312e1
Simone Traversari <simone.t.traversari@oracle.com>
2023-01-22 10:34:38 +0100

    Sample Commit Sun Jan 22 10:34:38 WEST 2023

--- variables.tf
+++ variables.tf
@@ -1,2 +1,10 @@
 variable "compartment_ocid" {}
 variable "region" {}
+
+variable "TF_VCN01_CONFIG" {
+  type = object({
+    cidr_block      = bool
+    dns_label       = string
+    display_name    = string
+  })
+}
--- vcn.tf
+++ vcn.tf
@@ -1,6 +1,6 @@
 resource "oci_core_virtual_network" "TF_VCN01" {
-  cidr_block = "10.0.0.0/16"
-  dns_label = "vcn01"
-  compartment_id = "${var.compartment_ocid}"
-  display_name = "vcn01"
+  cidr_block = "${var.TF_VCN01_CONFIG.cidr_block}"
+  dns_label = "${var.TF_VCN01_CONFIG.dns_label}"
+  compartment_id = "${var.compartment_ocid}"  
+  display_name = "${var.TF_VCN01_CONFIG.display_name}"
 }
