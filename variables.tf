4057aaf6ee0a5e1291e2dd4b3d0579e3a10bbe72
Simone Traversari <simone.t.traversari@oracle.com>
2023-01-22 10:49:35 +0100

    Sample Commit Sun Jan 22 10:49:35 WEST 2023

--- variables.tf
+++ variables.tf
@@ -4,3 +4,28 @@ variable "region" {}
 
 ### Custom
 variable "TF_VCN01_CONFIG" {}
+
+
+variable "image_id" {
+  type = string
+}
+
+variable "availability_zone_names" {
+  type    = list(string)
+  default = ["us-west-1a"]
+}
+
+variable "docker_ports" {
+  type = list(object({
+    internal = number
+    external = number
+    protocol = string
+  }))
+  default = [
+    {
+      internal = 8300
+      external = 8300
+      protocol = "tcp"
+    }
+  ]
+}
