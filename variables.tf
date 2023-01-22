67d9496591388bafd5bc3cbabe36b9e11712d44f
Simone Traversari <simone.t.traversari@oracle.com>
2023-01-22 11:05:47 +0100

    Sample Commit Sun Jan 22 11:05:47 WEST 2023

--- variables.tf
+++ variables.tf
@@ -3,17 +3,6 @@ variable "compartment_ocid" {}
 variable "region" {}
 
 ### Custom
-variable "TF_VCN01_CONFIG_CIDR_BLOCK" {
-  type = string
-  default = "10.0.0.0/16"
-}
-
-variable "TF_VCN01_CONFIG_DNS_LABEL" {
-  type = string
-  default = "vcn01"
-}
-
-variable "TF_VCN01_CONFIG_DISPLAY_NAME" {
-  type = string
-  default = "vcn01"
-}
+variable "TF_VCN01_CONFIG_CIDR_BLOCK" {}
+variable "TF_VCN01_CONFIG_DNS_LABEL" {}
+variable "TF_VCN01_CONFIG_DISPLAY_NAME" {}
