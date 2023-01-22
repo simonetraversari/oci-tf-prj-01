480109019566a91ae23850b02fc6f7a23cb62d91
Simone Traversari <simone.t.traversari@oracle.com>
2023-01-22 12:00:02 +0100

    Sample Commit Sun Jan 22 12:00:02 WEST 2023

--- variables.tf
+++ variables.tf
@@ -16,45 +16,45 @@ variable "TF_VCN01_SUBNET01_CONFIG_CIDR_BLOCK" {}
 variable "TF_VCN01_SUBNET01_CONFIG_DISPLAY_NAME" {}
 variable "TF_VCN01_SUBNET01_CONFIG_DNS_LABEL" {}
 
-variable "TF_VCN01_SECLIST01_CONFIG_DISPLAY_NAME" {}
-variable "TF_VCN01_SECLIST01_CONFIG_RULES" {
-  type = map(object({    
-    compartment_id  = string,
-    defined_tags    = map(string),
-    freeform_tags   = map(string),
-    ingress_rules   = list(object({
-      stateless     = bool,
-      protocol      = string,
-      src           = string,
-      src_type      = string,
-      src_port      = object({
-        min         = number,
-        max         = number
-      }),
-      dst_port      = object({
-        min         = number,
-        max         = number
-      }),
-      icmp_type     = number,
-      icmp_code     = number
-    })),
-    egress_rules    = list(object({
-      stateless     = bool,
-      protocol      = string,
-      dst           = string,
-      dst_type      = string,
-      src_port      = object({
-        min         = number,
-        max         = number
-      }),
-      dst_port      = object({
-        min         = number,
-        max         = number
-      }),
-      icmp_type     = number,
-      icmp_code     = number
-    }))
-  }))
-  description = "Parameters for customizing Security List(s)."
-  default = {}
-}
+###variable "TF_VCN01_SECLIST01_CONFIG_DISPLAY_NAME" {}
+###variable "TF_VCN01_SECLIST01_CONFIG_RULES" {
+###  type = map(object({    
+###    compartment_id  = string,
+###    defined_tags    = map(string),
+###    freeform_tags   = map(string),
+###    ingress_rules   = list(object({
+###      stateless     = bool,
+###      protocol      = string,
+###      src           = string,
+###      src_type      = string,
+###      src_port      = object({
+###        min         = number,
+###        max         = number
+###      }),
+###      dst_port      = object({
+###        min         = number,
+###        max         = number
+###      }),
+###      icmp_type     = number,
+###      icmp_code     = number
+###    })),
+###    egress_rules    = list(object({
+###      stateless     = bool,
+###      protocol      = string,
+###      dst           = string,
+###      dst_type      = string,
+###      src_port      = object({
+###        min         = number,
+###        max         = number
+###      }),
+###      dst_port      = object({
+###        min         = number,
+###        max         = number
+###      }),
+###      icmp_type     = number,
+###      icmp_code     = number
+###    }))
+###  }))
+###  description = "Parameters for customizing Security List(s)."
+###  default = {}
+###}
